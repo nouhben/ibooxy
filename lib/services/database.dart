1b7d01773b19676759de4821118d02f3dd7e282b
nouh ben kadi <benkadi.nouh@icloud.com>
2020-02-15 20:12:58 +0100

    ep 18 ==> firestore streams

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
+  final String uid;
+  DatabaseService({this.uid});
   //Collection refrrence
   final CollectionReference booxCollection =
       Firestore.instance.collection('booxs');
+
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await booxCollection.document(uid).setData({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength,
+    });
+  }
 }
